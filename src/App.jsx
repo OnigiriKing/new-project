26db4b5913b6e2556c186408a96274be557a7e94
OnigiriKing <oooNikita87@gmail.com>
2023-09-24 22:25:50 +0700

    Talwind works

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
 export default function App() {
   return (
     <div>
-      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-  <div class="pt-6 space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text">
-        Sarah Dayan
-      </div>
-      <div>
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="aspect-square bg-sky-500">
+              “Tailwind CSS is the only framework that I've seen scale on large
+              teams. It’s easy to customize, adapts to any design, and the build
+              size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text">Sarah Dayan</div>
+            <div>Staff Engineer, Algolia</div>
+          </figcaption>
+        </div>
+      </figure>
     </div>
   );
 }
