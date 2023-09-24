7a38c89f36bc79dc42a0d6909d47cc6b4e7146fd
OnigiriKing <oooNikita87@gmail.com>
2023-09-24 22:14:25 +0700

    Tailwind update (not working)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-
-
 export default function App() {
   return (
-    <div className="app">
-
+    <div>
+      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <div class="pt-6 space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text">
+        Sarah Dayan
+      </div>
+      <div>
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
     </div>
   );
 }
