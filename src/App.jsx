785b3a0424cbe67014e5337582390e99f63a84a3
OnigiriKing <oooNikita87@gmail.com>
2023-09-25 20:01:54 +0700

    folders added, app cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
+
+
 export default function App() {
   return (
-    <div>
-      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-        <div className="pt-6 space-y-4">
-          <blockquote>
-            <p className="aspect-square bg-sky-500">
-              “Tailwind CSS is the only framework that I've seen scale on large
-              teams. It’s easy to customize, adapts to any design, and the build
-              size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text">Sarah Dayan</div>
-            <div>Staff Engineer, Algolia</div>
-          </figcaption>
-        </div>
-      </figure>
+    <div class="flex">
+
     </div>
   );
 }
