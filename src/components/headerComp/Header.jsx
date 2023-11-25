a8b959b78da8a9e6a3a4b3ca9277336ff70378ef
OnigiriKing <oooNikita87@gmail.com>
2023-11-25 19:23:49 +0700

    Logo added

--- src/components/headerComp/Header.jsx
+++ src/components/headerComp/Header.jsx
@@ -1,19 +1,36 @@
 import { Link } from "react-router-dom";
+import logo from "utils/img/logo.png"
 
 export default function Header() {
   return (
     <div class="fixed flex justify-center w-full top-0 bg-black opacity-95  text-white">
-      <div class="flex items-center w-full max-w-7xl mx-20 justify-between py-8 ">
-        <img src="" alt="logo" />
-        <div>
-          <a href="">Home</a>
-          <a href="">About</a>
-          <a href="">Gallary</a>
-          <a href="">Schedule</a>
-          <a href="">Blog</a>
-          <a href="">Pricing</a>
-          <a href="">Classes</a>
-          <a href="">Contact</a>
+      <div class="flex items-center w-full max-w-7xl mx-20 justify-between py-8">
+        <img src={logo} alt="logo" class="w-24"/>
+        <div class="flex gap-6">
+          <a href="" class="hover:text-red-600">
+            Home
+          </a>
+          <a href="" class="hover:text-red-600">
+            About
+          </a>
+          <a href="" class="hover:text-red-600">
+            Gallary
+          </a>
+          <a href="" class="hover:text-red-600">
+            Schedule
+          </a>
+          <a href="" class="hover:text-red-600">
+            Blog
+          </a>
+          <a href="" class="hover:text-red-600">
+            Pricing
+          </a>
+          <a href="" class="hover:text-red-600">
+            Classes
+          </a>
+          <a href="" class="hover:text-red-600">
+            Contact
+          </a>
         </div>
         <div>
           <Link></Link>
