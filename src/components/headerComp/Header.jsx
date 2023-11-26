d5938863faa4ec022361ca5e57e0069172240432
OnigiriKing <oooNikita87@gmail.com>
2023-11-26 08:50:32 +0700

    header update svg added

--- src/components/headerComp/Header.jsx
+++ src/components/headerComp/Header.jsx
@@ -1,41 +1,42 @@
 import { Link } from "react-router-dom";
 import logo from "utils/img/logo.png";
+import allSvg from "svg/allSvg";
 
 export default function Header() {
   return (
     <div class="fixed flex justify-center w-full top-0 bg-black opacity-95  text-white">
-      <div class="flex items-center w-full max-w-7xl mx-20 justify-between py-2">
+      <div class="flex items-center w-full mx-16 justify-between py-2">
         <img src={logo} alt="logo" class="w-20" />
-        <div class="flex gap-6 transition-all duration-1000 ease-in-out">
-          <a href="" class="hover:text-red-600 ">
+        <div class="flex gap-6">
+          <Link href="" class="duration-500 hover:text-red-600 ">
             Home
-          </a>
-          <a href="" class="hover:text-red-600">
+          </Link>
+          <Link href="" class="duration-500 hover:text-red-600">
             About
-          </a>
-          <a href="" class="hover:text-red-600">
+          </Link>
+          <Link href="" class="duration-500 hover:text-red-600">
             Gallary
-          </a>
-          <a href="" class="hover:text-red-600">
+          </Link>
+          <Link href="" class="duration-500 hover:text-red-600">
             Schedule
-          </a>
-          <a href="" class="hover:text-red-600">
+          </Link>
+          <Link href="" class="duration-500 hover:text-red-600">
             Blog
-          </a>
-          <a href="" class="hover:text-red-600">
+          </Link>
+          <Link href="" class="duration-500 hover:text-red-600">
             Pricing
-          </a>
-          <a href="" class="hover:text-red-600">
+          </Link>
+          <Link href="" class="duration-500 hover:text-red-600">
             Classes
-          </a>
-          <a href="" class="hover:text-red-600">
+          </Link>
+          <Link href="" class="duration-500 hover:text-red-600">
             Contact
-          </a>
+          </Link>
         </div>
-        <div>
-          <Link></Link>
-          <div></div>
-          <Link>
+        <div class="flex gap-3">
+          <Link>{allSvg(25).user}</Link>
+          <div>{allSvg(25).about}</div>
+          <Link class="ml-4">
             <div></div>
             <h3>JOIN CLASS NOW</h3>
           </Link>
