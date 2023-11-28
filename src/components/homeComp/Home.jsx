63c4ffcc2f3b644c832dd8bdae46039a494afd4a
OnigiriKing <oooNikita87@gmail.com>
2023-11-28 17:33:13 +0700

    Home update

--- src/components/homeComp/Home.jsx
+++ src/components/homeComp/Home.jsx
@@ -1,11 +1,25 @@
-import gym from "./img/gym.jpg";
+
 
 export default function Home() {
   return (
-    <div class="flex center h-screen  bg-black pt-24 z-20">
+    <div class="flex center h-screen justify-end items-center  bg-black pt-24 z-20 text-white">
       <div>
         <img />
-        <div></div>
+        <div class="flex gap-16 mr-[10rem]">
+          <div>
+            <div>FIND YOUR ENERGY</div>
+            <div>
+              <h2>MAKE YOUR BODY</h2>
+              <h2>FIT & PERFECT</h2>
+            </div>
+            <div>Our classes</div>
+          </div>
+          <div class="flex flex-col">
+            <h3>Share</h3>
+            <div>___</div>
+            <div></div>
+          </div>
+        </div>
       </div>
     </div>
   );
