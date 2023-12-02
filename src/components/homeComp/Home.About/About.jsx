4d6bae39bc3fe22927cf504ee6686360ce6e271d
OnigiriKing <oooNikita87@gmail.com>
2023-12-02 23:29:43 +0700

    Welcome screen update

---
+++ src/components/homeComp/Home.About/About.jsx
@@ -0,0 +1,40 @@
+
+
+export default function About() {
+  return (
+    <div class="flex h-screen bg-white w-full justify-center">
+      <div class="w-full max-w-[70rem]">
+        <div class="flex">
+          <div>
+            <svg />
+            <h3>PROGRESSION</h3>
+            <p>
+              Our team of experts will work with you to create a customized plan
+              that helps you achieve success one step at a time.
+            </p>
+          </div>
+          <div>
+            <svg />
+            <h3>WORKOUT</h3>
+            <p>
+              With a variety of workouts to choose from, you'll have everything
+              you need to get into the best shape of your life.
+            </p>
+          </div>
+          <div>
+            <svg />
+            <h3>NUTRITIONS</h3>
+            <p>
+              Our team will work with you to create a personalized meal plan
+              that helps you reach your specific health goals.
+            </p>
+          </div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/homeComp/Home.jsx
+++ src/components/homeComp/Home.jsx
@@ -1,11 +1,12 @@
-
-import Welcome from "./Home.Welcome.jsx/Welcome";
+import Welcome from "./Home.Welcome/Welcome";
+import About from "./Home.About/About";
 
 export default function Home() {
 
   return (
     <div class="">
     <Welcome />
+    <About />
     </div>
   );
 }
