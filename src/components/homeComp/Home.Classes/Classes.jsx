222b69069e7255b209016dcb849f22f4d85b5e05
OnigiriKing <oooNikita87@gmail.com>
2023-12-07 07:48:15 +0700

    Classes "brush" fix

--- src/components/homeComp/Home.Classes/Classes.jsx
+++ src/components/homeComp/Home.Classes/Classes.jsx
@@ -2,15 +2,15 @@ import redBrush from "../img/redBrush.png";
 
 export default function Classes() {
   return (
-    <div class="flex center justify-end items-center  bg-white text-white">
-      <div class="py-2">
-        <div class="relative">
+    <div class="flex center justify-center items-center  bg-white text-white">
+      <div class="flex flex-col w-full max-w-[75rem] py-4 items-center">
+        <div class="relative w-[11rem] flex flex-col items-center">
           <h3 class="text-white relative z-[10] ">WHO WE ARE</h3>
-          <img class="absolute w-[11rem] -top-2 -left-6 z-[5]" src={redBrush} />
+          <img class="absolute -top-3  z-[5] " src={redBrush} />
         </div>
         <h3>We Offer Top-notch Flexible Classes</h3>
         <div>
-            <div></div>
+          <div></div>
         </div>
       </div>
     </div>
