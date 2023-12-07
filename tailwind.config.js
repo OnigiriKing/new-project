025f8db0d44c0894351bfeefad976c02cf9fa4f3
OnigiriKing <oooNikita87@gmail.com>
2023-12-07 08:54:32 +0700

    Classes screen update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "bg-class1": "url('utils/img/classesImg/class-1.png')",
+      },
+    },
   },
   plugins: [],
 };
