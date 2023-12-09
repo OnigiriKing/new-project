1581aaaf3642fe3eeb625e0a2ef527aaeee69e64
OnigiriKing <oooNikita87@gmail.com>
2023-12-09 14:01:11 +0700

    Other classes added

--- src/components/homeComp/Home.Classes/Classes.jsx
+++ src/components/homeComp/Home.Classes/Classes.jsx
@@ -11,6 +11,7 @@ export default function Classes() {
         </div>
         <h3>We Offer Top-notch Flexible Classes</h3>
         <div class="relative grid grid-cols-4 grid-flow-row grid-rows-2 gap-4 w-full h-full">
+          {/* classes */}
           <div class="class-item-wrap md:col-span-2 group relative">
             <div class="class-item-side group-hover:bg-white duration-[0.4s]">
               <p class="duration-[0.4s] px-3 py-2 rotate-[-30deg] text-white group-hover:text-red-600">
@@ -23,7 +24,87 @@ export default function Classes() {
             </div>
             <div
               class=" class-item
-            bg-bg-class1 group-hover:scale-[1] group-hover:grayscale-[0]"
+            bg-bg-class-1 group-hover:scale-[1] group-hover:grayscale-[0]"
+            ></div>
+          </div>
+          {/* class 2 */}
+          <div class="class-item-wrap md:col-span-1 group relative">
+            <div class="class-item-side group-hover:bg-white duration-[0.4s]">
+              <p class="duration-[0.4s] px-3 py-2 rotate-[-30deg] text-white group-hover:text-red-600">
+                {allSvg(50).lifting}
+              </p>
+            </div>
+            <div class="class-item-text">
+              <p class="text-2xl font-medium">Lifting</p>
+              <p class="bg-red-600">Wednesday: 9:00am-10:00am</p>
+            </div>
+            <div
+              class=" class-item
+            bg-bg-class-2 group-hover:scale-[1] group-hover:grayscale-[0]"
+            ></div>
+          </div>
+          {/* class 3 */}
+          <div class="class-item-wrap md:col-span-1 group relative">
+            <div class="class-item-side group-hover:bg-white duration-[0.4s]">
+              <p class="duration-[0.4s] px-3 py-2 rotate-[-30deg] text-white group-hover:text-red-600">
+                {allSvg(50).lifting}
+              </p>
+            </div>
+            <div class="class-item-text">
+              <p class="text-2xl font-medium">Lifting</p>
+              <p class="bg-red-600">Wednesday: 9:00am-10:00am</p>
+            </div>
+            <div
+              class=" class-item
+            bg-bg-class-3 group-hover:scale-[1] group-hover:grayscale-[0]"
+            ></div>
+          </div>
+          {/* class 4 */}
+          <div class="class-item-wrap md:col-span-1 group relative">
+            <div class="class-item-side group-hover:bg-white duration-[0.4s]">
+              <p class="duration-[0.4s] px-3 py-2 rotate-[-30deg] text-white group-hover:text-red-600">
+                {allSvg(50).lifting}
+              </p>
+            </div>
+            <div class="class-item-text">
+              <p class="text-2xl font-medium">Lifting</p>
+              <p class="bg-red-600">Wednesday: 9:00am-10:00am</p>
+            </div>
+            <div
+              class=" class-item
+            bg-bg-class-4 group-hover:scale-[1] group-hover:grayscale-[0]"
+            ></div>
+          </div>
+          {/* class 5 */}
+          <div class="class-item-wrap md:col-span-1 group relative">
+            <div class="class-item-side group-hover:bg-white duration-[0.4s]">
+              <p class="duration-[0.4s] px-3 py-2 rotate-[-30deg] text-white group-hover:text-red-600">
+                {allSvg(50).lifting}
+              </p>
+            </div>
+            <div class="class-item-text">
+              <p class="text-2xl font-medium">Lifting</p>
+              <p class="bg-red-600">Wednesday: 9:00am-10:00am</p>
+            </div>
+            <div
+              class=" class-item
+            bg-bg-class-5 group-hover:scale-[1] group-hover:grayscale-[0]"
+            ></div>
+          </div>
+          {/* class 6 */}
+          <div class="class-item-wrap md:col-span-2 group relative">
+            <div class="class-item-side group-hover:bg-white duration-[0.4s]">
+              <p class="duration-[0.4s] px-3 py-2 rotate-[-30deg] text-white group-hover:text-red-600">
+                {allSvg(50).lifting}
+              </p>
+            </div>
+            <div class="class-item-text">
+              <p class="text-2xl font-medium">Lifting</p>
+              <p class="bg-red-600">Wednesday: 9:00am-10:00am</p>
+            </div>
+            <div
+              class=" class-item
+            bg-bg-class-6 group-hover:scale-[1] group-hover:grayscale-[0]"
             ></div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "bg-class1": "url('utils/img/classesImg/class-1.png')",
+        "bg-class-1": "url('utils/img/classesImg/class-1.png')",
+        "bg-class-2": "url('utils/img/classesImg/class-2.png')",
+        "bg-class-3": "url('utils/img/classesImg/class-3.png')",
+        "bg-class-4": "url('utils/img/classesImg/class-4.png')",
+        "bg-class-5": "url('utils/img/classesImg/class-5.png')",
+        "bg-class-6": "url('utils/img/classesImg/class-6.png')",
       },
     },
   },
