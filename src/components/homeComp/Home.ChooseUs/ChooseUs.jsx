8dd55f4d978ab2381f35c725d6b8106266638adf
OnigiriKing <oooNikita87@gmail.com>
2023-12-18 22:07:38 +0700

    ChooseUs update

--- src/components/homeComp/Home.ChooseUs/ChooseUs.jsx
+++ src/components/homeComp/Home.ChooseUs/ChooseUs.jsx
@@ -1,13 +1,14 @@
 import allSvg from "svg/allSvg";
+import { Link } from "react-router-dom";
 
 
-export default function Welcome() {
+export default function ChooseUs() {
   return (
     <div class="flex center h-[107vh] justify-end items-center  bg-black pt-24 text-white">
       <div class="flex flex-col common-wrapper items-center gap-8 mx-4">
         <div>
-          <h3></h3>
-          <Link class="self-start ml-10 font-medium text-white bg-gray-700 px-8 py-4 group hover:bg-red-600 duration-500 flex items-center gap-2">
+          <h3>We Are Always Providing Best Fitness Service For You</h3>
+          <Link class="self-start ml-10 font-medium text-black bg-white px-8 py-4 group flex items-center gap-2 justify-center w-fit">
             JOIN US {allSvg(20).arrow}
           </Link>
         </div>
@@ -27,7 +28,7 @@ export default function Welcome() {
                 <h5></h5>
                 <div></div>
               </div>
-              <Link class="self-start ml-10 font-medium text-black bg-white px-8 py-4 group hover:bg-red-600 duration-500 flex items-center gap-2">
+              <Link class="self-start ml-10 font-medium text-white bg-gray-700 px-8 py-4 group hover:bg-red-600 duration-500 flex items-center gap-2 justify-center w-fit">
                 OUR CLASSES {allSvg(20).arrow}
               </Link>
             </div>
--- src/components/homeComp/Home.jsx
+++ src/components/homeComp/Home.jsx
@@ -1,14 +1,16 @@
 import Welcome from "./Home.Welcome/Welcome";
 import About from "./Home.About/About";
 import Classes from "./Home.Classes/Classes";
+import ChooseUs from "./Home.ChooseUs/ChooseUs";
 
 export default function Home() {
 
   return (
     <div class="">
-    <Welcome />
-    <About />
-    <Classes />
+      <Welcome />
+      <About />
+      <Classes />
+      <ChooseUs />
     </div>
   );
 }
