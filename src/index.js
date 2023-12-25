ed5a029b7edfe44691ec92ecd3747d5078646f5f
OnigiriKing <oooNikita87@gmail.com>
2023-12-25 21:13:09 +0700

    redux installed and set

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./dist/style.css";
 import "./style/costomClass.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import store from "features/Redux/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
