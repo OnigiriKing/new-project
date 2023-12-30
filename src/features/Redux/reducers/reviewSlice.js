41cab4e653ca5a7aa4e66b04e895a0e7f956f979
OnigiriKing <oooNikita87@gmail.com>
2023-12-30 20:03:50 +0700

    reviewSlice created

---
+++ src/features/Redux/reducers/reviewSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = 1;
+
+const reviewSlice = createSlice({
+    name: "reviewCount",
+    initialState,
+    reducers: {
+        next: (state, action) => {
+            if (state < 3) {
+                state++
+            } else {
+                state = 1;
+            }
+        },
+        prev: (state, action) => {
+            if (state > 1) {
+                state--
+            } else {
+                state = 3;
+            }
+        }
+    }
+})
+
+export default reviewSlice;
