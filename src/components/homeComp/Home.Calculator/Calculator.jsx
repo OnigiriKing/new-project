c9144b13159fc56cd5115fc00a174fadc821ecbf
OnigiriKing <oooNikita87@gmail.com>
2023-12-31 14:45:53 +0700

    Calculator is created

---
+++ src/components/homeComp/Home.Calculator/Calculator.jsx
@@ -0,0 +1,26 @@
+export default function Calculator() {
+
+  return;
+  <div class="flex justify-center items-center  bg-white  my-6">
+    <div class="flex flex-col common-wrapper items-center gap-8">
+      <div>
+        <h3>
+          Let's Calculate Your <b>BMI</b>
+        </h3>
+        <p>
+          Easily determine your body mass index with our accurate calculation
+          tool.
+        </p>
+        <div>
+          <div></div>
+          <div>
+            <h4>Your BMI is:</h4>
+            <h4>Your weight is:</h4>
+          </div>
+          <button>CALCULATE</button>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  </div>;
+}
