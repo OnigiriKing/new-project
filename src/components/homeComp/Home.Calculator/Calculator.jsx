c1e3087d27115afc8ec319f6263f6a9ecbe1de2f
OnigiriKing <oooNikita87@gmail.com>
2024-01-01 20:54:47 +0700

    Calculator style update

--- src/components/homeComp/Home.Calculator/Calculator.jsx
+++ src/components/homeComp/Home.Calculator/Calculator.jsx
@@ -1,26 +1,29 @@
 export default function Calculator() {
   return (
-  <div class="flex justify-center items-center  bg-white  my-6">
-    <div class="flex flex-col common-wrapper items-center gap-8">
-      <div>
-        <h3>
-          Let's Calculate Your <b>BMI</b>
-        </h3>
-        <p>
-          Easily determine your body mass index with our accurate calculation
-          tool.
-        </p>
-        <div>
-          <div></div>
+    <div class="flex justify-center items-center  bg-black text-white  my-6">
+      <div class="flex flex-col common-wrapper items-center gap-8">
+        <div class="flex flex-col">
+          <h3>
+            Let's Calculate Your <b>BMI</b>
+          </h3>
+          <p>
+            Easily determine your body mass index with our accurate calculation
+            tool.
+          </p>
           <div>
-            <h4>Your BMI is:</h4>
-            <h4>Your weight is:</h4>
+            <div class="flex gap-4">
+              <input placeholder="weight / kg" class="px-4 py-2"></input>
+              <input placeholder="Height / sm" class="px-4 py-2"></input>
+            </div>
+            <div class="flex gap-4">
+              <h4>Your BMI is:</h4>
+              <h4>Your weight is:</h4>
+            </div>
+            <button>CALCULATE</button>
           </div>
-          <button>CALCULATE</button>
         </div>
+        <div></div>
       </div>
-      <div></div>
     </div>
-  </div>
-  )
+  );
 }
