be5b1e7a763922f8f008d90798375b742c540460
OnigiriKing <oooNikita87@gmail.com>
2024-01-02 18:21:56 +0700

    Style update

--- src/components/homeComp/Home.Calculator/Calculator.jsx
+++ src/components/homeComp/Home.Calculator/Calculator.jsx
@@ -2,20 +2,26 @@ export default function Calculator() {
   return (
     <div class="flex justify-center items-center  bg-black text-white  my-6">
       <div class="flex flex-col common-wrapper gap-8 py-14">
-        <div class="flex flex-col gap-4">
-          <h3 class="text-3xl">
+        <div class="flex flex-col gap-5">
+          <h3 class="text-4xl">
             Let's Calculate Your <b class="text-red-600">BMI</b>
           </h3>
           <p>
             Easily determine your body mass index with our accurate calculation
             tool.
           </p>
-          <div class="flex flex-col gap-4">
+          <div class="flex flex-col gap-5">
             <div class="flex gap-4">
-              <input placeholder="weight / kg" class="px-4 py-2"></input>
-              <input placeholder="Height / sm" class="px-4 py-2"></input>
+              <input
+                placeholder="Weight / kg"
+                class="px-4 py-2 bg-black border-neutral-500 border-2"
+              ></input>
+              <input
+                placeholder="Height / cm"
+                class="px-4 py-2 bg-black border-neutral-500 border-2"
+              ></input>
             </div>
-            <div class="flex gap-4">
+            <div class="flex gap-5">
               <h4>Your BMI is:</h4>
               <h4>Your weight is:</h4>
             </div>
