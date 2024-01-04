dd59a85067e061b00e7c8981d6f4e995d04a4ab7
OnigiriKing <oooNikita87@gmail.com>
2024-01-04 19:18:28 +0700

    index

--- src/features/Redux/reducers/indexSlice.js
+++ src/features/Redux/reducers/indexSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  ibm: 0,
+  bmi: 0,
   weight: "",
 };
 
@@ -11,7 +11,23 @@ const indexSlice = createSlice({
   reducers: {
     calculate: (state, action) => {
       const { weight, height } = action.payload;
-      
+      const bmi = weight / (height / 100) ** 2;
+      if (bmi < 18.5) {
+        const status = "Underweight";
+      } else if (bmi >= 18.5 && bmi <= 24.9) {
+        const status = "Healthy Weight";
+      } else if (bmi >= 25.0 && bmi <= 29.9) {
+        const status = "Overweight";
+      } else if (bmi >= 30.0) {
+        const status = "Obesity";
+      } else {
+        const status = "Invalid BMI";
+      }
+
+      return {
+        bmi: bmi,
+        weight: status,
+      };
     },
   },
 });
