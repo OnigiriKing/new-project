1d186ee4bbe704d791261fdc931856b4292fb212
OnigiriKing <oooNikita87@gmail.com>
2024-01-12 19:05:11 +0700

    Small style update

--- src/components/homeComp/Home.Chart/Chart.jsx
+++ src/components/homeComp/Home.Chart/Chart.jsx
@@ -3,6 +3,31 @@ import redBrush from "../img/redBrush.png";
 import test from "./img/Jogging.jpg"
 
 export default function Chart() {
+
+  function TestComp() {
+
+    return (
+      <div class="flex flex-col group relative w-[25rem] h-[40rem] z-[1] bg-red-50">
+        <img
+          src={test}
+          class=" grayscale-[1] duration-[.4s]
+            bg-bg-class-4 group-hover:grayscale-[0]"
+        ></img>
+        <div class="flex flex-col w-full p-2 mt-5">
+          <h3 class="bg-white absolute top-[44%] right-[10rem] px-6 py-2 font-bold">
+            LEVEL
+          </h3>
+          <div class="text-3xl text-gray-600">
+            $ <b class="text-5xl text-black">40</b> p/m
+          </div>
+          <p class="mt-4">For beginers</p>
+          <Link class="button-class">PURCHASE NOW</Link>
+        </div>
+      </div>
+    );
+  }
+
+
   return (
     <div class="flex justify-center items-center  bg-white  my-6">
       <div class="flex flex-col common-wrapper items-center gap-8 text-center py-2">
@@ -13,24 +38,10 @@ export default function Chart() {
         <h3></h3>
         <p></p>
         {/* Price */}
-        <div class="flex items-center bg-red-50">
-          <div class="flex flex-col group relative w-[30rem] h-[40rem] z-[1]">
-            <img
-              src={test}
-              class=" grayscale-[1] duration-[.4s]
-            bg-bg-class-4 group-hover:grayscale-[0]"
-            ></img>
-            <div class="flex flex-col w-full p-2 mt-5">
-              <h3 class="bg-white absolute top-[44%] right-[7rem] px-6 py-2 font-bold">
-                LEVEL
-              </h3>
-              <div class="text-3xl text-gray-600">
-                $ <b class="text-5xl text-black">40</b> p/m
-              </div>
-              <p class="mt-4">For beginers</p>
-              <Link class="button-class">PURCHASE NOW</Link>
-            </div>
-          </div>
+        <div class="gap-10 grid grid-cols-3 items-center">
+          <TestComp />
+          <TestComp />
+          <TestComp />
         </div>
       </div>
     </div>
