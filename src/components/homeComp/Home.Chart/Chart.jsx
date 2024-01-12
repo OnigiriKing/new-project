2d174f754b2a4e8913194dd401b774b8c18fc028
OnigiriKing <oooNikita87@gmail.com>
2024-01-12 19:14:35 +0700

    Chart style update

--- src/components/homeComp/Home.Chart/Chart.jsx
+++ src/components/homeComp/Home.Chart/Chart.jsx
@@ -7,21 +7,21 @@ export default function Chart() {
   function TestComp() {
 
     return (
-      <div class="flex flex-col group relative w-[25rem] h-[40rem] z-[1] bg-red-50">
+      <div class="flex flex-col group relative w-[25rem] h-[40rem] z-[1] shadow-lg">
         <img
           src={test}
           class=" grayscale-[1] duration-[.4s]
             bg-bg-class-4 group-hover:grayscale-[0]"
         ></img>
         <div class="flex flex-col w-full p-2 mt-5">
-          <h3 class="bg-white absolute top-[44%] right-[10rem] px-6 py-2 font-bold">
+          <h3 class="bg-white absolute top-[43%] right-1/2 translate-x-1/2 px-6 py-2 font-bold">
             LEVEL
           </h3>
           <div class="text-3xl text-gray-600">
             $ <b class="text-5xl text-black">40</b> p/m
           </div>
-          <p class="mt-4">For beginers</p>
-          <Link class="button-class">PURCHASE NOW</Link>
+          <p class="mt-4 h-[6rem]">For beginners</p>
+          <Link class="button-class self-center mt-10">PURCHASE NOW</Link>
         </div>
       </div>
     );
