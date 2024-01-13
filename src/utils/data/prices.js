6a8fde4bcb5606ab24eeb5eaf42226b26b75c6fb
OnigiriKing <oooNikita87@gmail.com>
2024-01-13 23:09:54 +0700

    Price added

---
+++ src/utils/data/prices.js
@@ -0,0 +1,25 @@
+
+
+
+const prices = {
+  beginer: {
+    img: "img",
+    level: "Beginer",
+    price: 30,
+    des: "Perfect for beginers",
+  },
+  advanced: {
+    img: "img",
+    level: "Advanced",
+    price: 45,
+    des: "Suitable for expirienced athlets",
+  },
+  pro: {
+    img: "img",
+    level: "Professional",
+    price: 70,
+    des: "Only for professionals",
+  },
+};
+
+export default prices
