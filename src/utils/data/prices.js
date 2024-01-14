4ecea7a3e5d168bc4e353d56866791039e0ed8cc
OnigiriKing <oooNikita87@gmail.com>
2024-01-14 21:39:26 +0700

    prices updates, images added

--- src/utils/data/prices.js
+++ src/utils/data/prices.js
@@ -1,21 +1,23 @@
-
+import beginer from "utils/img/chart/beginer.png"
+import advanced from "utils/img/chart/advanced.png";
+import pro from "utils/img/chart/pro.png";
 
 
 const prices = {
   beginer: {
-    img: "img",
+    img: beginer,
     level: "Beginer",
     price: 30,
     des: "Perfect for beginers",
   },
   advanced: {
-    img: "img",
+    img: advanced,
     level: "Advanced",
     price: 45,
     des: "Suitable for expirienced athlets",
   },
   pro: {
-    img: "img",
+    img: pro,
     level: "Professional",
     price: 70,
     des: "Only for professionals",
