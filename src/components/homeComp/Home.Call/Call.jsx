09fc9254032d4c581a84af003dde6c142b75918b
OnigiriKing <oooNikita87@gmail.com>
2024-01-15 21:30:50 +0700

    Call comp updated

--- src/components/homeComp/Home.Call/Call.jsx
+++ src/components/homeComp/Home.Call/Call.jsx
@@ -1,9 +1,8 @@
 import { Link } from "react-router-dom";
-import redBrush from "../img/redBrush.png";
 
 export default function Call() {
   return (
-    <div class="flex justify-center items-center  bg-white  my-6">
+    <div class="flex justify-center items-center  bg-black text-white  my-6">
       <div class="flex flex-col common-wrapper items-center gap-8 text-center py-2">
         <div>
           <h3>Need a Fitness Trainer?</h3>
@@ -11,6 +10,9 @@ export default function Call() {
             <b>Call:</b>+66-000-000
           </h3>
           <Link class="button-class">PURCHASE NOW</Link>
+        </div>
+        <div>
+
         </div>
         <div></div>
       </div>
