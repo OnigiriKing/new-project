5d8eb325330564eaddfd8e79bb2a7c7137dbb497
OnigiriKing <oooNikita87@gmail.com>
2024-01-16 13:06:09 +0700

    News component added

--- src/components/homeComp/Home.News/News.jsx
+++ src/components/homeComp/Home.News/News.jsx
@@ -1,17 +1,12 @@
 import { Link } from "react-router-dom";
 
-export default function Call() {
+export default function News() {
   return (
     <div class="flex justify-center items-center  bg-black text-white  my-6">
       <div class="flex common-wrapper items-center gap-8 text-center py-[4rem]">
-        <div>
-          <h3>Need a Fitness Trainer?</h3>
-          <h3>
-            <b>Call:</b>+66-000-000
-          </h3>
-          <Link class="button-class">PURCHASE NOW</Link>
-        </div>
-        <div>
+        <div class="mb-4 mt-[5rem] relative w-[13rem] flex flex-col items-center">
+          <p class="z-[6] font-bold text-white">LATEST BLOG</p>
+          <img class="absolute -top-[1rem]  z-[5] " src={redBrush} />
         </div>
       </div>
     </div>
