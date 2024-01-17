6e2c21108654d7e356cc6f34eebdc49459758a32
OnigiriKing <oooNikita87@gmail.com>
2024-01-17 09:54:28 +0700

    Style update

--- src/components/homeComp/Home.Call/Call.jsx
+++ src/components/homeComp/Home.Call/Call.jsx
@@ -3,16 +3,15 @@ import { Link } from "react-router-dom";
 export default function Call() {
   return (
     <div class="flex justify-center items-center  bg-black text-white  my-6">
-      <div class="flex common-wrapper items-center gap-8 text-center py-[4rem]">
-        <div>
-          <h3>Need a Fitness Trainer?</h3>
-          <h3>
-            <b>Call:</b>+66-000-000
+      <div class="flex common-wrapper items-center gap-[1rem] text-center py-[4rem]">
+        <div class="z-10 flex flex-col gap-[1rem] items-start font-bold">
+          <h3 class="text-3xl">Need a Fitness Trainer?</h3>
+          <h3 class="text-2xl flex gap-1">
+            <b class="text-red-600">Call:</b>+66-000-000
           </h3>
-          <Link class="button-class">PURCHASE NOW</Link>
+          <Link class="button-class mt-4">PURCHASE NOW</Link>
         </div>
-        <div>
-        </div>
+        <div></div>
       </div>
     </div>
   );
