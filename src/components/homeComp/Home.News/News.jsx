22a3a1d7986e27da2b87ee3c582a8fb697feafab
OnigiriKing <oooNikita87@gmail.com>
2024-01-18 00:46:25 +0700

    News component update

--- src/components/homeComp/Home.News/News.jsx
+++ src/components/homeComp/Home.News/News.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
+import redBrush from "../img/redBrush.png";
 
 export default function News() {
   return (
-    <div class="flex justify-center items-center  bg-black text-white  my-6">
-      <div class="flex common-wrapper items-center gap-8 text-center py-[4rem]">
+    <div class="flex justify-center items-center  bg-white text-black  my-6">
+      <div class="flex flex-col common-wrapper items-center gap-8 text-center py-[4rem]">
         <div class="mb-4 mt-[5rem] relative w-[13rem] flex flex-col items-center">
           <p class="z-[6] font-bold text-white">LATEST BLOG</p>
           <img class="absolute -top-[1rem]  z-[5] " src={redBrush} />
         </div>
+        <div>
+          <h3>Our Recent News</h3>
+          <p>
+            Gymat an unknown printer took a galley of type and scrambled make a
+            type specimen book.
+          </p>
+        </div>
+        <div></div>
       </div>
     </div>
   );
--- src/components/homeComp/Home.jsx
+++ src/components/homeComp/Home.jsx
@@ -7,6 +7,7 @@ import Testimonials from "./Home.Testimonials/Testimonials";
 import Calculator from "./Home.Calculator/Calculator";
 import Chart from "./Home.Chart/Chart";
 import Call from "./Home.Call/Call";
+import News from "./Home.News/News";
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <Testimonials />
       <Calculator />
       <Chart />
+      <News />
       <Call />
     </div>
   );
