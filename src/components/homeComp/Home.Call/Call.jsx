bf5bb80ed5e4607015ecbb8360484f00a90d2c6d
OnigiriKing <oooNikita87@gmail.com>
2024-01-17 10:23:32 +0700

    Style update, img added

--- src/components/homeComp/Home.Call/Call.jsx
+++ src/components/homeComp/Home.Call/Call.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
+import ropes from "utils/img/ropes.png"
 
 export default function Call() {
   return (
     <div class="flex justify-center items-center  bg-black text-white  my-6">
-      <div class="flex common-wrapper items-center gap-[1rem] text-center py-[4rem]">
-        <div class="z-10 flex flex-col gap-[1rem] items-start font-bold">
+      <div class="flex common-wrapper items-center text-center py-[4rem] h-[20rem] relative">
+        <div class="z-10 flex flex-col gap-[1rem] items-start font-bold w-[70%]">
           <h3 class="text-3xl">Need a Fitness Trainer?</h3>
           <h3 class="text-2xl flex gap-1">
             <b class="text-red-600">Call:</b>+66-000-000
           </h3>
           <Link class="button-class mt-4">PURCHASE NOW</Link>
         </div>
-        <div></div>
+        <div class="overflow-hidden h-[20rem] w-[55rem] absolute -right-[9rem]">
+          <img class="relative bottom-[19rem]" src={ropes}></img>
+        </div>
       </div>
     </div>
   );
