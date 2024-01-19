e5324d1b2fb09ecaf16cbeb1525256e098663e4f
OnigiriKing <oooNikita87@gmail.com>
2024-01-19 08:51:27 +0700

    Article component is created

--- src/components/homeComp/Home.News/News.Article/Article.jsx
+++ src/components/homeComp/Home.News/News.Article/Article.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import allSvg from "svg/allSvg";
 
-export default function Article() {
+export default function Article({date, name, des}) {
   return (
-    <div class="flex justify-center items-center">
-    <div class="">
-
-    </div>
+    <div class="flex justify-center items-center shadow-lg">
+      <div class="">
+        <div>date</div>
+        <h3>name</h3>
+        <p>des</p>
+        <Link class="button-class">READ MORE {allSvg(20).arrow}</Link>
+      </div>
     </div>
   );
 }
