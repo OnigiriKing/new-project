06d8ec693a92c6332981c4893239d6e4b2ff8cba
OnigiriKing <oooNikita87@gmail.com>
2024-01-21 00:21:24 +0700

    Article component is ready

--- src/components/homeComp/Home.News/News.Article/Article.jsx
+++ src/components/homeComp/Home.News/News.Article/Article.jsx
@@ -6,12 +6,10 @@ export default function Article({date, name, des}) {
     <div class="flex justify-center items-center shadow-lg">
       <div class="flex flex-col gap-4 p-[2rem] items-start">
         <div class="border-[1px] border-black p-1 px-2 rounded-full font-bold">
-          19.01.2024
+          {date}
         </div>
-        <h3 class="font-bold text-2xl">name</h3>
-        <p class="text-gray-600 h-[6rem] text-left">
-          Learn how to ease back into your CrossFit routine after a vacation with tips and strategies for success.
-        </p>
+        <h3 class="font-bold text-2xl">{name}</h3>
+        <p class="text-gray-600 min-h-[6rem] text-left">{des}</p>
         <Link class="button-class">READ MORE {allSvg(20).arrow}</Link>
       </div>
     </div>
