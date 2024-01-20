6d983ae9724a5c4086ff4c90b14af38f0e78bbce
OnigiriKing <oooNikita87@gmail.com>
2024-01-20 09:28:27 +0700

    Article update

--- src/components/homeComp/Home.News/News.Article/Article.jsx
+++ src/components/homeComp/Home.News/News.Article/Article.jsx
@@ -5,7 +5,7 @@ export default function Article({date, name, des}) {
   return (
     <div class="flex justify-center items-center shadow-lg">
       <div class="flex flex-col gap-4 p-[4rem] items-start">
-        <div>19.01.2024</div>
+        <div class="border-[1px] border-black p-1 px-2 rounded-full font-bold">19.01.2024</div>
         <h3>name</h3>
         <p>des</p>
         <Link class="button-class">READ MORE {allSvg(20).arrow}</Link>
