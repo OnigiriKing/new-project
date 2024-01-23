9990f42d3bc62ceb93dfc1332c59e3f6976b1c49
OnigiriKing <oooNikita87@gmail.com>
2024-01-24 00:19:22 +0700

    TopComp update

--- src/components/aboutComp/About.jsx
+++ src/components/aboutComp/About.jsx
@@ -1,8 +1,14 @@
-
+import TopComp from "components/topComp/TopComp";
 
 export default function About() {
   return (
     <div class="">
+    <TopComp des="About" />
+    <div></div>
+    <div>
+      <div></div>
+      <div></div>
+    </div>
     </div>
   );
 }
--- src/components/topComp/TopComp.jsx
+++ src/components/topComp/TopComp.jsx
@@ -3,9 +3,9 @@
 export default function TopComp({des}) {
   return (
     <div class="">
-      <div class="bg-black text-white">
+      <div class="bg-black text-white mt-[4.8rem] h-[15rem] w-full flex flex-col items-center justify-end">
         <img></img>
-        <h3>{des}</h3>
+        <h3 class="mb-8 text-3xl font-bold">{des}</h3>
       </div>
     </div>
   );
