3248d3bc1588718c36171a8daa2e854334160c5f
OnigiriKing <oooNikita87@gmail.com>
2024-01-23 22:25:53 +0700

    TopComp created

---
+++ src/components/topComp/TopComp.jsx
@@ -0,0 +1,12 @@
+
+
+export default function TopComp({des}) {
+  return (
+    <div class="">
+      <div class="bg-black text-white">
+        <img></img>
+        <h3>{des}</h3>
+      </div>
+    </div>
+  );
+}
