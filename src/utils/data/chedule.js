146fbc43156ada76917f62f64cbffab20e1886c8
OnigiriKing <oooNikita87@gmail.com>
2024-02-01 15:48:59 +0700

    scheduleList created (need to check)

---
+++ src/utils/data/chedule.js
@@ -0,0 +1,10 @@
+const scheduleList = {
+    class1: {
+        day: "monday",
+        name: "Crosfit",
+        time: "10.00am - 10.00am",
+        trainer: "Jason Smith"
+    }
+}
+
+export default scheduleList;
