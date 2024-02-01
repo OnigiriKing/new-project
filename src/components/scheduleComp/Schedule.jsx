07530d6d334b4e15f639fd0ea18d63f99e8f4aaa
OnigiriKing <oooNikita87@gmail.com>
2024-02-01 15:45:50 +0700

    Small update

--- src/components/scheduleComp/Schedule.jsx
+++ src/components/scheduleComp/Schedule.jsx
@@ -5,7 +5,12 @@ export default function Schedule() {
     <div class="w-full flex flex-col items-center">
       <TopComp des="Schedule" />
       <div class="common-wrapper pt-[4rem]">
-        
+        <div>
+          <div></div>
+        </div>
+        <div>
+
+        </div>
       </div>
     </div>
   );
