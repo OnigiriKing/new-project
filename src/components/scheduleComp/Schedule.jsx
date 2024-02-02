63a93468080cd0b855632a4e68642b541b00ef59
OnigiriKing <oooNikita87@gmail.com>
2024-02-02 08:45:09 +0700

    State and mapping added

--- src/components/scheduleComp/Schedule.jsx
+++ src/components/scheduleComp/Schedule.jsx
@@ -1,6 +1,10 @@
 import TopComp from "components/topComp/TopComp";
+import React from "react";
+import scheduleList from "utils/data/chedule";
 
 export default function Schedule() {
+  const [day, setDay] = React.useState("monday");
+
   return (
     <div class="w-full flex flex-col items-center">
       <TopComp des="Schedule" />
@@ -9,7 +13,13 @@ export default function Schedule() {
           <div></div>
         </div>
         <div>
-
+          {Object.entries(scheduleList[day]).map(([key, data]) => (
+            <div key={key}>
+              <h3>{data.name}</h3>
+              <p>Time: {data.time}</p>
+              <p>Trainer: {data.trainer}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
