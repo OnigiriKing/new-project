c1dc3503b4535922f845aa7da12835fe840e12b1
OnigiriKing <oooNikita87@gmail.com>
2024-02-02 08:39:35 +0700

    shedule list created

--- src/utils/data/chedule.js
+++ src/utils/data/chedule.js
@@ -1,10 +1,104 @@
 const scheduleList = {
+  monday: {
     class1: {
-        day: "monday",
-        name: "Crosfit",
-        time: "10.00am - 10.00am",
-        trainer: "Jason Smith"
-    }
-}
+      name: "Crossfit",
+      time: "8:00am - 9:00am",
+      trainer: "Jason Smith",
+    },
+    class2: {
+      name: "Yoga",
+      time: "10:00am - 11:00am",
+      trainer: "Angela Lee",
+    },
+    class3: {
+      name: "HIIT",
+      time: "12:00pm - 1:00pm",
+      trainer: "Mark Johnson",
+    },
+    class4: {
+      name: "Spinning",
+      time: "6:00pm - 7:00pm",
+      trainer: "Susan Clark",
+    },
+  },
+  tuesday: {
+    class1: {
+      name: "Crossfit",
+      time: "9:00am - 10:00am",
+      trainer: "Jason Smith",
+    },
+    class2: {
+      name: "Pilates",
+      time: "11:00am - 12:00pm",
+      trainer: "Nancy Drew",
+    },
+    class3: {
+      name: "Boxing",
+      time: "1:00pm - 2:00pm",
+      trainer: "Derek Hale",
+    },
+  },
+  wednesday: {
+    class1: {
+      name: "Crossfit",
+      time: "7:00am - 8:00am",
+      trainer: "Jason Smith",
+    },
+    class2: {
+      name: "Body Pump",
+      time: "10:00am - 11:00am",
+      trainer: "Chris Evans",
+    },
+    class3: {
+      name: "Aerobics",
+      time: "12:00pm - 1:00pm",
+      trainer: "Olivia Brown",
+    },
+    class4: {
+      name: "Dance Fitness",
+      time: "6:00pm - 7:00pm",
+      trainer: "Emily Wright",
+    },
+  },
+  thursday: {
+    class1: {
+      name: "Crossfit",
+      time: "8:00am - 9:00am",
+      trainer: "Jason Smith",
+    },
+    class2: {
+      name: "Kickboxing",
+      time: "11:00am - 12:00pm",
+      trainer: "Brian O'Connor",
+    },
+    class3: {
+      name: "Power Yoga",
+      time: "1:00pm - 2:00pm",
+      trainer: "Nicole Lee",
+    },
+    class4: {
+      name: "TRX",
+      time: "5:00pm - 6:00pm",
+      trainer: "Aaron Smith",
+    },
+  },
+  friday: {
+    class1: {
+      name: "Crossfit",
+      time: "6:00am - 7:00am",
+      trainer: "Jason Smith",
+    },
+    class2: {
+      name: "Spin Class",
+      time: "9:00am - 10:00am",
+      trainer: "Hannah White",
+    },
+    class3: {
+      name: "Barre",
+      time: "11:00am - 12:00pm",
+      trainer: "Megan Foster",
+    },
+  },
+};
 
 export default scheduleList;
