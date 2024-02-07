cd7b3e5796e2e4a65c74d548fdc969a6a5269e36
OnigiriKing <oooNikita87@gmail.com>
2024-02-07 21:18:12 +0700

    ClassseComp update

--- src/components/classesComp/ClassesComp.jsx
+++ src/components/classesComp/ClassesComp.jsx
@@ -1,14 +1,30 @@
 import TopComp from "components/topComp/TopComp";
 import React from "react";
+import allSvg from "svg/allSvg";
 
 export default function ClassesComp() {
-
-
   return (
     <div class="w-full flex flex-col items-center">
       <TopComp des="Classes" />
       <div class="common-wrapper pt-[4rem]">
-
+        <div>
+          <div class="bg-black text-white w-[20rem]">
+            <img></img>
+            <div>
+            <h3>Cycling</h3>
+            <div>
+              <p>Dorian Yate</p>
+              <p>Wed: 9:00 am</p>
+            </div>
+            <button class="button-class-light">
+              JOIN NOW {allSvg(20).arrow}
+            </button>
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-center">
+          <button class="button-class">SHOW MORE</button>
+        </div>
       </div>
     </div>
   );
