2f5f24211aa6b8a16f0e7c4850f488fc96c2a53e
OnigiriKing <oooNikita87@gmail.com>
2024-02-12 15:07:00 +0700

    Classes added

---
+++ src/utils/data/classes.js
@@ -0,0 +1,32 @@
+const classes = {
+  class1: {
+    name: "Cycling",
+    trainer: "Dorian Yates",
+    time: "Wednesday at 9:00 am",
+  },
+  class2: {
+    name: "HIIT Bootcamp",
+    trainer: "Jillian Michaels",
+    time: "Monday at 6:00 pm",
+  },
+  class3: {
+    name: "Power Yoga",
+    trainer: "Rodney Yee",
+    time: "Friday at 8:00 am",
+  },
+  class4: {
+    name: "Boxing Fundamentals",
+    trainer: "Freddie Roach",
+    time: "Tuesday and Thursday at 7:00 pm",
+  },
+  class5: {
+    name: "Strength Training",
+    trainer: "Mark Rippetoe",
+    time: "Monday, Wednesday, and Friday at 12:00 pm",
+  },
+  class6: {
+    name: "Zumba Fitness",
+    trainer: "Beto Perez",
+    time: "Saturday at 10:00 am",
+  },
+};
