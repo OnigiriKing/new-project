bac5846910f6a1e3488c577cbadfbacc5b0c55aa
OnigiriKing <oooNikita87@gmail.com>
2024-02-22 15:23:08 +0700

    Blog update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import SchedulePage from "pages/Schedule/SchedulePage";
 import ClassesPage from "pages/ClassesPage/ClassesPage";
 import ContactPage from "pages/Contact/ContactPage";
+import BlogPage from "pages/Blog/BlogPage";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/schedule" element={<SchedulePage />} />
+        <Route path="/blog" element={<BlogPage />} />
         <Route path="/classes" element={<ClassesPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
--- src/pages/Blog/BlogPage.js
+++ src/pages/Blog/BlogPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Blog from "components/BlogComp";
+import Blog from "components/blogComp/Blog";
 
 export default function BlogPage() {
   return (
