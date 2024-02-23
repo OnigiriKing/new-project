b05fd4c73916443493d39e869e36248f2f4c57b6
OnigiriKing <oooNikita87@gmail.com>
2024-02-23 16:39:00 +0700

    Blog structure update

--- src/components/blogComp/Blog.jsx
+++ src/components/blogComp/Blog.jsx
@@ -6,7 +6,19 @@ export default function Blog() {
   return (
     <div class="w-full flex flex-col items-center">
       <TopComp des="Blog" />
-      <div class="common-wrapper pt-[4rem]"></div>
+      <div class="common-wrapper pt-[4rem]">
+        <div></div>
+        <div>
+          <div>
+            <input></input>
+            <button></button>
+          </div>
+          <div>Categories</div>
+          <div>Recent Posts</div>
+          <div>Tags</div>
+          <div>ad</div>
+        </div>
+      </div>
     </div>
   );
 }
