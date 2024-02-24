5b022c51a56cc1616399da03438246a0174d32de
OnigiriKing <oooNikita87@gmail.com>
2024-02-24 08:47:57 +0700

    style update

--- src/components/blogComp/Blog.jsx
+++ src/components/blogComp/Blog.jsx
@@ -6,12 +6,15 @@ export default function Blog() {
   return (
     <div class="w-full flex flex-col items-center">
       <TopComp des="Blog" />
-      <div class="common-wrapper pt-[4rem]">
-        <div></div>
-        <div>
-          <div>
-            <input></input>
-            <button></button>
+      <div class="common-wrapper pt-[4rem] flex">
+        <div class="w-[70%]"></div>
+        <div class="w-[30%]">
+          <div class="flex">
+            <input
+              placeholder="Search..."
+              class="px-[.7rem] py-[1rem] w-[20rem] border-[1px] border-gray-200 rounded-md"
+            ></input>
+            <button>search</button>
           </div>
           <div>Categories</div>
           <div>Recent Posts</div>
