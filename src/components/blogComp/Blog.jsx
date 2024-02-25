6d9b622450a52d8dd73218babf3052409b020adb
OnigiriKing <oooNikita87@gmail.com>
2024-02-25 08:46:38 +0700

    Blog update

--- src/components/blogComp/Blog.jsx
+++ src/components/blogComp/Blog.jsx
@@ -2,6 +2,14 @@ import TopComp from "components/topComp/TopComp";
 import allSvg from "svg/allSvg";
 
 export default function Blog() {
+  const blogCat = {
+    cat1: { name: "Fitness", number: 3 },
+    cat2: { name: "Yoga", number: 4 },
+    cat3: { name: "Meditation", number: 2 },
+    cat4: { name: "Boxing", number: 5 },
+    cat5: { name: "Body Building", number: 3 },
+    cat6: { name: "Crossfit", number: 2 },
+  };
   return (
     <div class="w-full flex flex-col items-center">
       <TopComp des="Blog" />
@@ -18,7 +26,19 @@ export default function Blog() {
               {allSvg(35).serach}
             </button>
           </div>
-          <div>Categories</div>
+          <div class="bg-gray-100 p-[2rem]">
+            <h3 class="mb-[1rem]">Categories</h3>
+            {Object.keys(blogCat).map((el) => {
+              const category = blogCat[el];
+
+              return (
+                <div class="flex justify-between">
+                  <h3>{category.name}</h3>
+                  <h4>{category.number}</h4>
+                </div>
+              );
+            })}
+          </div>
           <div>Recent Posts</div>
           <div>Tags</div>
           <div>ad</div>
