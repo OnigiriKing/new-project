a64a7320fac2762fec5683fe42c818b2bf2ee85e
OnigiriKing <oooNikita87@gmail.com>
2024-02-26 20:17:14 +0700

    tags update

--- src/components/blogComp/Blog.jsx
+++ src/components/blogComp/Blog.jsx
@@ -17,7 +17,7 @@ export default function Blog() {
     "#Meditation",
     "#Boxing",
     "#BodyBuilding",
-    "#Crossfit"
+    "#Crossfit",
   ];
   return (
     <div class="w-full flex flex-col items-center">
@@ -54,18 +54,19 @@ export default function Blog() {
             })}
           </div>
           <div>Recent Posts</div>
-          <div class="bg-gray-100 p-[2rem]">
+          <div class="bg-gray-100 p-[2rem] ">
             <h3 class="mb-[1rem] contact-page-header text-[1.2rem]">
               Popular Tags
             </h3>
-            {tagsList.map((el) => {
-
-              return (
-                <div class="flex mt-[1rem] pb-[1rem] border-b-[1px] text-gray-500 border-gray-300 cursor-pointer hover:text-red-600 duration-[.4s]">
-                  <h3>{el}</h3>
-                </div>
-              );
-            })}
+            <div class="flex flex-wrap gap-[.5rem] ">
+              {tagsList.map((el) => {
+                return (
+                  <h3 class="text-gray-500 cursor-pointer hover:text-red-600 duration-[.4s] ">
+                    {el}
+                  </h3>
+                );
+              })}
+            </div>
           </div>
           <div>ad</div>
         </div>
