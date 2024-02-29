3469408f9179e10e59f1bbd75ec0ba3a5df099be
OnigiriKing <oooNikita87@gmail.com>
2024-02-29 18:55:30 +0700

    Blog data update (type update)

--- src/utils/data/blogData.js
+++ src/utils/data/blogData.js
@@ -8,12 +8,12 @@ const blogCat = {
 };
 
 const tagsList = [
-  "#Fitness",
-  "#Yoga",
-  "#Meditation",
-  "#Boxing",
-  "#BodyBuilding",
-  "#Crossfit",
+  "Fitness",
+  "Yoga",
+  "Meditation",
+  "Boxing",
+  "BodyBuilding",
+  "Crossfit",
 ];
 
 const blogPost = {
@@ -21,37 +21,43 @@ const blogPost = {
     date: "19.01.24",
     name: "Summer Body Bootcamp",
     author: "Admin",
+    type: "Fitness",
     des: "Join our intense six-week bootcamp to get your body summer-ready. Focused on high-intensity workouts and nutritional guidance, it's perfect for all fitness levels.",
   },
   post2: {
     date: "20.01.24",
     name: "Family Fitness Fun Day",
     author: "Admin",
+    type: "Fitness",
     des: "A day of fun and fitness for the whole family! Enjoy group activities, healthy snacks, and learn how to keep your family active and healthy together.",
   },
   post3: {
     date: "21.01.24",
     name: "Strength Training 101",
     author: "Admin",
+    type: "Body Building",
     des: "New to strength training? This beginner's course will teach you the fundamentals of lifting weights safely and effectively, setting you up for a lifetime of strength.",
   },
   post4: {
     date: "22.01.24",
     name: "Marathon Prep Made Easy",
     author: "Admin",
+    type: "Crossfit",
     des: "Get ready for your next marathon with our comprehensive training program. Covering everything from running techniques to nutrition, we'll help you cross that finish line.",
   },
   post5: {
     date: "23.01.24",
     name: "Mindful Meditation and Movement",
-    des: "Discover the power of mindfulness combined with gentle movement in this relaxing class. Ideal for stress relief and improving overall well-being.",
     author: "Admin",
+    type: "Meditation",
+    des: "Discover the power of mindfulness combined with gentle movement in this relaxing class. Ideal for stress relief and improving overall well-being.",
   },
   post6: {
     date: "24.01.24",
     name: "Nutrition for Optimal Performance",
-    des: "Our expert dietitian will guide you through the basics of sports nutrition, helping you fuel your body for your best performance yet.",
     author: "Admin",
+    type: "Fitness",
+    des: "Our expert dietitian will guide you through the basics of sports nutrition, helping you fuel your body for your best performance yet.",
   },
 };
 
