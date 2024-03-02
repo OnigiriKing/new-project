5737fba6b7330cf565fd6f928a76c9f02a5f615a
OnigiriKing <oooNikita87@gmail.com>
2024-03-02 18:25:48 +0700

    Posts.js update

--- src/components/blogComp/Blog.Posts/Posts.js
+++ src/components/blogComp/Blog.Posts/Posts.js
@@ -1,15 +1,24 @@
-import { tagsList } from "utils/data/blogData";
+import { blogPost } from "utils/data/blogData";
 
 export default function Posts() {
   return (
     <div class="bg-gray-100 p-[2rem] ">
       <h3 class="mb-[1rem] contact-page-header text-[1.2rem]">Recent Posts</h3>
       <div class="flex flex-wrap gap-[.5rem] ">
-        {tagsList.map((el) => {
+        {Object.keys(blogPost).map((el) => {
+          const post = blogPost[el];
           return (
-            <h3 class="cursor-pointer hover:text-red-600 text-gray-600 duration-[.4s] bg-white px-[.4rem] py-[.2rem] ">
-              {"#" + el}
-            </h3>
+            <div class="h-[8rem] flex">
+              <div>
+                <img></img>
+              </div>
+              <div class="flex flex-col gap-[.5rem] h-full justify-center">
+                <p class="text-gray-600">{post.date}</p>
+                <h3 class="cursor-pointer hover:text-red-600 duration-[.4s]  font-bold">
+                  {post.name}
+                </h3>
+              </div>
+            </div>
           );
         })}
       </div>
