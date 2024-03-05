9c76e563b184967bc229ff92ae5e4c63de90c9cc
OnigiriKing <oooNikita87@gmail.com>
2024-03-05 20:12:09 +0700

    Posts comp update

--- src/components/blogComp/Blog.Posts/Posts.jsx
+++ src/components/blogComp/Blog.Posts/Posts.jsx
@@ -1,4 +1,5 @@
 import { blogPost } from "utils/data/blogData";
+import allSvg from "svg/allSvg";
 
 export default function Posts() {
   return (
@@ -7,11 +8,13 @@ export default function Posts() {
         const post = blogPost[el];
         return (
           <div class="h-[8rem] flex items-center gap-[1rem]">
-            <img></img>
-            <p></p>
-            <h3></h3>
-            <p></p>
-            <button></button>
+            <img src={post.img}></img>
+            <p>
+              By {post.author} | {post.date} | {post.date}
+            </p>
+            <h3>{post.name}</h3>
+            <p>{post.des}</p>
+            <button>PURCHASE NOW {allSvg(20).arrow}</button>
           </div>
         );
       })}
