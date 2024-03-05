522b916c4044cb2fed8e329a5a178b2aa6a38bc2
OnigiriKing <oooNikita87@gmail.com>
2024-03-05 20:19:33 +0700

    Style update

--- src/components/blogComp/Blog.Posts/Posts.jsx
+++ src/components/blogComp/Blog.Posts/Posts.jsx
@@ -3,20 +3,22 @@ import allSvg from "svg/allSvg";
 
 export default function Posts() {
   return (
-    <div>
+    <div class="flex flex-col gap-[2.5rem] w-[90%]">
       {Object.keys(blogPost).map((el) => {
         const post = blogPost[el];
         return (
           <div class="flex flex-col gap-[1rem]">
-            <div class="h-[30rem]">
-              <img class="h-full object-fill" src={post.img}></img>
+            <div class="h-[25rem]">
+              <img class="h-full object-cover w-full" src={post.img}></img>
             </div>
             <p>
               By <b>{post.author}</b> | {post.date} | {post.type}
             </p>
             <h3>{post.name}</h3>
             <p>{post.des}</p>
-            <button>PURCHASE NOW {allSvg(20).arrow}</button>
+            <button class="button-class mt-[1rem]">
+              PURCHASE NOW {allSvg(20).arrow}
+            </button>
           </div>
         );
       })}
