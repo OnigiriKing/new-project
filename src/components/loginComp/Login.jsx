0fcc6f5116726aa53a210d393f3d04b75ecd24bc
OnigiriKing <oooNikita87@gmail.com>
2024-03-10 19:19:43 +0700

    Login comp started

---
+++ src/components/loginComp/Login.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Login() {
+  return (
+    <div class="w-full flex flex-col items-center">
+      <TopComp des="About" />
+      <div class="common-wrapper pt-[4rem]">
+      
+      </div>
+    </div>
+  );
+}
