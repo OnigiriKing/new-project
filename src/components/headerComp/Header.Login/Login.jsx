a84d826e8e75ff26d4252468612c6f0dced6b0d0
OnigiriKing <oooNikita87@gmail.com>
2024-03-12 21:40:48 +0700

    Login update

--- src/components/headerComp/Header.Login/Login.jsx
+++ src/components/headerComp/Header.Login/Login.jsx
@@ -1,8 +1,19 @@
 export default function Login() {
   return (
     <div class="w-[30rem] flex flex-col items-center absolute h-[100vh] bg-white right-0">
-      <div class="common-wrapper pt-[4rem]">
-
+      <div class="common-wrapper pt-[4rem] text-black">
+        <h3 class="text-black">Sign Up</h3>
+        <div>
+          <div>
+            <label>Email</label>
+            <input placeholder="bbkGym@gmail.com"></input>
+          </div>
+          <button>Sign Up</button>
+          <div>
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
       </div>
     </div>
   );
