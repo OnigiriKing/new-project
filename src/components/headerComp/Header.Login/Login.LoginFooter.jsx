69ac3e076b27be3652e776681439ff7ae58731be
OnigiriKing <oooNikita87@gmail.com>
2024-03-20 19:54:06 +0700

    LoginFooter added

---
+++ src/components/headerComp/Header.Login/Login.LoginFooter.jsx
@@ -0,0 +1,48 @@
+
+
+export default function LoginFooter({ state }) {
+  if (state === "Sign In") {
+    return (
+      <div>
+        <h3>
+          Create a new account:{" "}
+          <b
+            class="text-red-600 cursor-pointer"
+            onClick={() => setLoginState("Sign Up")}
+          >
+            Sign Up
+          </b>
+        </h3>
+        <h4 class="flex gap-[.3rem] mt-[0.5rem]">
+          Test account: (Login: admin, password: admin12)
+        </h4>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h3>
+          Already have account?{" "}
+          <b
+            class="text-red-600 cursor-pointer"
+            onClick={() => setLoginState("Sign In")}
+          >
+            Sign In
+          </b>
+        </h3>
+        <h4 class="flex gap-[.3rem] mt-[0.5rem]">
+          (Make <p class="text-red-600 cursor-pointer">new Accout</p> or go to
+          <p
+            class="text-red-600 cursor-pointer"
+            onClick={() => setLoginState("Sign In")}
+          >
+            Sign In
+          </p>
+          Page for Test Account)
+        </h4>
+      </div>
+    );
+  }
+}
+
+
--- src/components/headerComp/Header.Login/Login.jsx
+++ src/components/headerComp/Header.Login/Login.jsx
@@ -2,6 +2,7 @@ import { setLogin } from "features/Redux/reducers/loginWindowSlice";
 import { useSelector, useDispatch } from "react-redux";
 import allSvg from "svg/allSvg";
 import { useState } from "react";
+import LoginFooter from "./Login.LoginFooter";
 
 export default function Login() {
   const loginWindow = useSelector((state) => state.loginWindow);
@@ -11,50 +12,6 @@ export default function Login() {
 
 
 
-  function LoginFooter({ state }) {
-    if (state === "Sign In") {
-      return (
-        <div>
-          <h3>
-            Create a new account:{" "}
-            <b
-              class="text-red-600 cursor-pointer"
-              onClick={() => setLoginState("Sign Up")}
-            >
-              Sign Up
-            </b>
-          </h3>
-          <h4 class="flex gap-[.3rem] mt-[0.5rem]">
-            Test account: (Login: admin, password: admin12)
-          </h4>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h3>
-            Already have account?{" "}
-            <b
-              class="text-red-600 cursor-pointer"
-              onClick={() => setLoginState("Sign In")}
-            >
-              Sign In
-            </b>
-          </h3>
-          <h4 class="flex gap-[.3rem] mt-[0.5rem]">
-            (Make <p class="text-red-600 cursor-pointer">new Accout</p> or go to
-            <p
-              class="text-red-600 cursor-pointer"
-              onClick={() => setLoginState("Sign In")}
-            >
-              Sign In
-            </p>
-            Page for Test Account)
-          </h4>
-        </div>
-      );
-    }
-  }
 
   return (
     <div
