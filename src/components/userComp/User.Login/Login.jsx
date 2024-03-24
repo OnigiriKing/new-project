dadbfc0cc1072315b364d4c47a29dc7e9434238f
OnigiriKing <oooNikita87@gmail.com>
2024-03-24 10:13:53 +0700

    Login update (scroll bug)

--- src/components/userComp/User.Login/Login.jsx
+++ src/components/userComp/User.Login/Login.jsx
@@ -14,42 +14,27 @@ export default function Login() {
 
 
   return (
-    <div
-      class={`w-[30rem] flex flex-col items-center absolute h-[100vh] bg-white duration-[.5s] ease-in-out z-[100] ${
-        loginWindow ? "right-0" : "right-[-100%]"
-      } `}
-    >
-      <div
-        class="cursor-pointer hover:text-red-600 duration-[.3s] ease-in-out text-black absolute left-[1.5rem] top-[1.5rem]"
-        onClick={() => dispatch(setLogin(false))}
-      >
-        {allSvg(35).close}
-      </div>
-      <div class=" pt-[6rem] text-black w-[90%]">
-        <h3 class="text-black text-center font-bold text-2xl mb-[2rem]">
-          {lognState}
-        </h3>
-        <div class="flex flex-col gap-[1.5rem]">
-          <div class="flex flex-col">
-            <label class="text-lg">Email</label>
-            <input
-              placeholder="BbkGym@gmail.com"
-              class="input-style mt-[0.4rem]"
-            ></input>
-          </div>
-          <div class="flex flex-col">
-            <label class="text-lg">Password</label>
-            <input
-              placeholder="Password"
-              class="input-style mt-[0.4rem]"
-            ></input>
-          </div>
-          <button class="bg-red-600 text-white py-[.6rem]">{lognState}</button>
-          <div class="">
-            <LoginFooter state={lognState} setLoginState={setLoginState} />
-          </div>
+    <>
+      <h3 class="text-black text-center font-bold text-2xl mb-[2rem]">
+        {lognState}
+      </h3>
+      <div class="flex flex-col gap-[1.5rem]">
+        <div class="flex flex-col">
+          <label class="text-lg">Email</label>
+          <input
+            placeholder="BbkGym@gmail.com"
+            class="input-style mt-[0.4rem]"
+          ></input>
+        </div>
+        <div class="flex flex-col">
+          <label class="text-lg">Password</label>
+          <input placeholder="Password" class="input-style mt-[0.4rem]"></input>
+        </div>
+        <button class="bg-red-600 text-white py-[.6rem]">{lognState}</button>
+        <div class="">
+          <LoginFooter state={lognState} setLoginState={setLoginState} />
         </div>
       </div>
-    </div>
+    </>
   );
 }
