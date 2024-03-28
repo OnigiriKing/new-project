0068a099d6ea292e59ed17dd01628c5bbfbb3608
OnigiriKing <oooNikita87@gmail.com>
2024-03-28 17:06:06 +0700

    Bug fix

--- src/components/userComp/User.Login/Login.LoginFooter.jsx
+++ src/components/userComp/User.Login/Login.LoginFooter.jsx
@@ -3,47 +3,39 @@ import { useDispatch, useSelector } from "react-redux";
 
 export default function LoginFooter() {
 
-  const loginWindow = useSelector((state) => state.loginWindow);
+  const loginStatus = useSelector((state) => state.loginStatus);
   const dispatch = useDispatch();
 
-  if (loginWindow.type === "Sign In") {
-    return (
-      <div>
-        <h3>
-          Create a new account:
-          <b
-            class="text-red-600 cursor-pointer"
-            onClick={() => dispatch()}
-          >
-            Sign Up
-          </b>
-        </h3>
-        <h4 class="flex gap-[.3rem] mt-[0.5rem]">
-          Test account: (Login: admin, password: admin12)
-        </h4>
-      </div>
-    );
-  } else {
+  if (loginStatus.type === "Sign Up") {
     return (
       <div>
         <h3>
           Already have account?{" "}
-          <b
-            class="text-red-600 cursor-pointer"
-            onClick={() => dispatch()}
-          >
+          <b class="text-red-600 cursor-pointer" onClick={() => dispatch()}>
             Sign In
           </b>
         </h3>
         <h4 class="flex gap-[.3rem] mt-[0.5rem]">
           (Make <p class="text-red-600 cursor-pointer">new Accout</p> or go to
-          <p
-            class="text-red-600 cursor-pointer"
-            onClick={() => dispatch()}
-          >
+          <p class="text-red-600 cursor-pointer" onClick={() => dispatch()}>
             Sign In
           </p>
           Page for Test Account)
+        </h4>
+      </div>
+    );
+  } else {
+    
+    return (
+      <div>
+        <h3>
+          Create a new account:
+          <b class="text-red-600 cursor-pointer" onClick={() => dispatch()}>
+            Sign Up
+          </b>
+        </h3>
+        <h4 class="flex gap-[.3rem] mt-[0.5rem]">
+          Test account: (Login: admin, password: admin12)
         </h4>
       </div>
     );
