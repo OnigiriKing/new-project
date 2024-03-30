45b98c88ede3c0f047687bbcd2d35a91e56f2b0e
OnigiriKing <oooNikita87@gmail.com>
2024-03-30 11:57:02 +0700

    userSlice update, logOut added

--- src/features/Redux/reducers/userSlice.js
+++ src/features/Redux/reducers/userSlice.js
@@ -3,27 +3,34 @@ import usersData from "utils/data/usersData";
 
 const initialState = JSON.parse(localStorage.getItem("users")) || usersData;
 
-
 const userInfoSlice = createSlice({
   name: "userInfo",
   initialState,
   reducers: {
     registerUser: (state, action) => {
       const { email, password } = action.payload;
-      if (!state.users[email]) { 
+      if (!state.users[email]) {
         state.users[email] = { email, password };
-        localStorage.setItem("users", JSON.stringify(state)); 
+        localStorage.setItem("users", JSON.stringify(state));
       }
-  },
-  loginUser: (state, action) => {
+    },
+    loginUser: (state, action) => {
       const { email, password } = action.payload;
       if (state.users[email] && state.users[email].password === password) {
         state.currentUser = email;
         state.isLoggedIn = true;
-        localStorage.setItem("users", JSON.stringify(state))
+        localStorage.setItem("users", JSON.stringify(state));
       }
+    },
+    logOut: (state, action) => {
+      if (state.isLoggedIn === true) {
+        state.currentUser = "";
+        state.isLoggedIn = false;
+        localStorage.setItem("users", JSON.stringify(state));
       }
-}});
+    },
+  },
+});
 
-export const { registerUser, loginUser } = userInfoSlice.actions;
+export const { registerUser, loginUser, logOut } = userInfoSlice.actions;
 export default userInfoSlice.reducer;
