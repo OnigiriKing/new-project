c3407862016a1c38e381afc66e859f3aa1135ae3
OnigiriKing <oooNikita87@gmail.com>
2024-04-04 22:37:23 +0700

    Login update

--- src/components/userComp/User.LoggedIn/LoggedIn.jsx
+++ src/components/userComp/User.LoggedIn/LoggedIn.jsx
@@ -1,9 +1,11 @@
 import { logOut } from "features/Redux/reducers/userSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function LoggedIn() {
 
   const dispatch = useDispatch()
+  const userName = useSelector((state) => state.userInfo.currentUser);
+
 
 
   return (
