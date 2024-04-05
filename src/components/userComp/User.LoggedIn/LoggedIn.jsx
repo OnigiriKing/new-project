8eddfa75a09139d68624f1dd84aeffae417756f2
OnigiriKing <oooNikita87@gmail.com>
2024-04-05 14:47:12 +0700

    LoginIcon added

--- src/components/userComp/User.LoggedIn/LoggedIn.jsx
+++ src/components/userComp/User.LoggedIn/LoggedIn.jsx
@@ -1,5 +1,6 @@
 import { logOut } from "features/Redux/reducers/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import allSvg from "svg/allSvg";
 
 export default function LoggedIn() {
 
@@ -10,6 +11,10 @@ export default function LoggedIn() {
 
   return (
     <>
+    <div>
+    <div>{allSvg(20).userIcon}</div>
+    <h3>{userName}</h3>
+    </div>
       <button onClick={() => dispatch(logOut())}>Log Out</button>
     </>
   );
