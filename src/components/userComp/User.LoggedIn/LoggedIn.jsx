6ba63296a7e162ea84bf26368344392b049031a6
OnigiriKing <oooNikita87@gmail.com>
2024-04-08 09:26:22 +0700

    bug fixed, storage is cleared

--- src/components/userComp/User.LoggedIn/LoggedIn.jsx
+++ src/components/userComp/User.LoggedIn/LoggedIn.jsx
@@ -6,16 +6,20 @@ export default function LoggedIn() {
 
   const dispatch = useDispatch()
   const curentUser = useSelector((state) => state.userInfo.currentUser);
-  const userName = useSelector((state) => state.userInfo[curentUser].name);
+  const userName = useSelector(
+    (state) => state.userInfo.users[curentUser].name
+  );
+
 
 
 
   return (
     <>
-    <div>
-    <div>{allSvg(20).userIcon}</div>
-    <h3>{userName}</h3>
-    </div>
+      <div>
+        <div>{allSvg(20).userIcon}</div>
+        <h3>{curentUser}</h3>
+        <h3>{userName}</h3>
+      </div>
       <button onClick={() => dispatch(logOut())}>Log Out</button>
     </>
   );
