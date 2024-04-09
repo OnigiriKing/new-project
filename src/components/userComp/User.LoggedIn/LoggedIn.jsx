2c7363cb05a13a07ac5919517e458c647980648d
OnigiriKing <oooNikita87@gmail.com>
2024-04-10 00:25:03 +0700

    pop up update

--- src/components/userComp/User.LoggedIn/LoggedIn.jsx
+++ src/components/userComp/User.LoggedIn/LoggedIn.jsx
@@ -1,38 +1,42 @@
 import { logOut } from "features/Redux/reducers/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import allSvg from "svg/allSvg";
+import { useState } from "react";
 
 export default function LoggedIn() {
 
   const dispatch = useDispatch()
   const userName = useSelector((state) => state.userInfo.currentUser.name);
 
+  const [logOutConfirm, setlogOutConfirm] = useState(false)
+
   function logOutHandler() {
-    const isConfirmed = window.confirm("Are you sure you want to log out?");
-    if (isConfirmed) {
       dispatch(logOut());
-    }
+      setlogOutConfirm(false)
   }
+
 
   function LogOutWindow() {
 
     return (
-    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-6 rounded-lg shadow-md">
-        <p className="mb-4">Are you sure you want to log out?</p>
-        <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
-        >
-          Yes
-        </button>
-        <button
-          className="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded"
-        >
-          No
-        </button>
+      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <p className="mb-4">Are you sure you want to log out?</p>
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
+            onClick={() => logOutHandler()}
+          >
+            Yes
+          </button>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded"
+            onClick={() => setlogOutConfirm(false)}
+          >
+            No
+          </button>
+        </div>
       </div>
-    </div>
-    )
+    );
   };
 
 
@@ -48,7 +52,7 @@ export default function LoggedIn() {
         <h3>{userName}</h3>
       </div>
       <button
-        onClick={() => logOutHandler()}
+        onClick={() => setlogOutConfirm(true)}
         class="hover:text-red-600 duration-[.4s]"
       >
         Log Out
