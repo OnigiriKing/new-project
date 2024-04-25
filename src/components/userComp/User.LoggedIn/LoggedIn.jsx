f2b723399ce40df7e271d04c986ad33f699b9dda
OnigiriKing <oooNikita87@gmail.com>
2024-04-25 21:00:33 +0700

    style update

--- src/components/userComp/User.LoggedIn/LoggedIn.jsx
+++ src/components/userComp/User.LoggedIn/LoggedIn.jsx
@@ -43,45 +43,50 @@ export default function LoggedIn() {
 
   function IconChangeWindow() {
     return (
-      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-[10]">
+      <div
+        className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-[10]"
+
+      >
         <div className="bg-white p-6 rounded-lg shadow-md justify-center">
           <div class="flex gap-[1rem] flex-col">
-          <div onClick={() => setChooseIcon(false)}
-          class="self-end cursor-pointer hover:text-red-600 duration-[.3s]">
-          {allSvg(30).close}
-          </div>
-          <div class="flex flex-row gap-[1rem]">
-            <div className="w-[6rem] cursor-pointer">
-              <img
-                src={profilePics.pp1}
-                onClick={() => {
-                  dispatch(changePic({ newPic: profilePics.pp1 }));
-                  setChooseIcon(false);
-                }}
-                className="w-fit"
-              ></img>
+            <div
+              onClick={() => setChooseIcon(false)}
+              class="self-end cursor-pointer hover:text-red-600 duration-[.3s]"
+            >
+              {allSvg(30).close}
             </div>
-            <div className="w-[6rem] cursor-pointer">
-              <img
-                src={profilePics.pp2}
-                onClick={() => {
-                  dispatch(changePic({ newPic: profilePics.pp2 }));
-                  setChooseIcon(false);
-                }}
-                className="w-fit"
-              ></img>
+            <div class="flex flex-row gap-[1rem]">
+              <div className="w-[6rem] cursor-pointer">
+                <img
+                  src={profilePics.pp1}
+                  onClick={() => {
+                    dispatch(changePic({ newPic: profilePics.pp1 }));
+                    setChooseIcon(false);
+                  }}
+                  className="w-fit"
+                ></img>
+              </div>
+              <div className="w-[6rem] cursor-pointer">
+                <img
+                  src={profilePics.pp2}
+                  onClick={() => {
+                    dispatch(changePic({ newPic: profilePics.pp2 }));
+                    setChooseIcon(false);
+                  }}
+                  className="w-fit"
+                ></img>
+              </div>
+              <div className="w-[5rem] cursor-pointer">
+                <img
+                  src={profilePics.pp3}
+                  onClick={() => {
+                    dispatch(changePic({ newPic: profilePics.pp3 }));
+                    setChooseIcon(false);
+                  }}
+                  className="w-fit"
+                ></img>
+              </div>
             </div>
-            <div className="w-[5rem] cursor-pointer">
-              <img
-                src={profilePics.pp3}
-                onClick={() => {
-                  dispatch(changePic({ newPic: profilePics.pp3 }));
-                  setChooseIcon(false);
-                }}
-                className="w-fit"
-              ></img>
-            </div>
-          </div>
           </div>
         </div>
       </div>
@@ -104,7 +109,13 @@ export default function LoggedIn() {
               {allSvg(25).changeIcon}
             </div>
           </div>
-          <h3>{userName}</h3>
+          <h3>Hello, {userName}!</h3>
+          <h3 class="hover:text-red-600 duration-[.4s] cursor-pointer">
+            Change password
+          </h3>
+          <h3 class="hover:text-red-600 duration-[.4s] cursor-pointer">
+            Change email
+          </h3>
         </div>
         <button
           onClick={() => setlogOutConfirm(true)}
