d5234c3a7ca749c20579ba6bdfd180183931b220
OnigiriKing <oooNikita87@gmail.com>
2024-05-29 22:22:59 +0700

    info.jsx begining

---
+++ src/components/infoComp/Info.jsx
@@ -0,0 +1,28 @@
+
+import allSvg from "svg/allSvg";
+import { useDispatch, useSelector } from "react-redux";
+import { setLogin } from "features/Redux/reducers/loginWindowSlice";
+
+export default function User() {
+  const dispatch = useDispatch();
+  const infoWindow = useSelector((state) => state.infoWindow);
+  const logInStatus = useSelector((state) => state.userInfo.isLoggedIn);
+
+  return (
+    <div
+      class={`w-[30rem] flex flex-col items-center fixed h-[100vh] bg-white duration-[.5s] ease-in-out z-[100] ${
+        infoWindow ? "right-0" : "right-[-100%]"
+      } `}
+    >
+      <div
+        class="cursor-pointer hover:text-red-600 duration-[.3s] ease-in-out text-black absolute left-[1.5rem] top-[1.5rem]"
+        onClick={() => dispatch(setLogin(false))}
+      >
+        {allSvg(35).close}
+      </div>
+      <div class=" pt-[6rem] text-black w-[90%]">
+
+      </div>
+    </div>
+  );
+}
