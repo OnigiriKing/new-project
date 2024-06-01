95f19a424c2a22b440c3288d11260473e442912c
OnigiriKing <oooNikita87@gmail.com>
2024-06-01 10:56:53 +0700

    slice update

--- src/features/Redux/reducers/loginWindowSlice.js
+++ src/features/Redux/reducers/loginWindowSlice.js
@@ -1,12 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = true;
+const initialState = {
+  login: true,
+  info: true,
+};
 
 const loginWindowSlice = createSlice({
   name: "loginWindow",
   initialState,
   reducers: {
     setLogin: (state, action) => action.payload,
+    setInfo: (state, action) => action.payload,
   },
 });
 
